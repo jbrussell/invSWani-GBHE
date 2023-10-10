4703a4709ccee430cd11c578cf001a16499771c0
Josh Russell <jbrussell93@gmail.com>
2023-10-10 19:42:35 -0400

    use circular statistics
    The old way worked but required the user to be careful about angle wrapping. The new codes do proper circular statistics on anisotropy fast directions when estimating median and confidence intervals, taking into account 2-theta and 4-theta repeating.

---
+++ run_MINEOS/functions/get_95_68_prctile_circ.m
@@ -0,0 +1,43 @@
+function [ x_med, x_u95, x_l95, x_u68, x_l68 ] = get_95_68_prctile_circ( x, fld )
+% [ x_med, x_u95, x_l95, x_u68, x_l68 ] = get_95_68_prctile_circ( x, fld )
+%
+% Get the upper and low 95th and 68th percentiles and median;
+%
+% This version calculates the circular median and 1 and 2 sigma
+%
+% x input in degrees
+%
+
+if contains(fld,'E') || contains(fld,'4')
+    th = 4;
+else
+    th = 2;
+end
+
+x_med = circ_median(th*x*pi/180) * 180/pi / th;
+% x_u95 = (x_med + circ_confmean(th*x*pi/180,1-0.95)'* 180/pi / th);
+% x_l95 = (x_med - circ_confmean(th*x*pi/180,1-0.95)'* 180/pi / th);
+% x_u68 = (x_med + circ_confmean(th*x*pi/180,1-0.68)'* 180/pi / th);
+% x_l68 = (x_med - circ_confmean(th*x*pi/180,1-0.68)'* 180/pi / th);
+
+x_u95 = (x_med + 2*circ_std(th*x*pi/180)'* 180/pi /th);
+x_l95 = (x_med - 2*circ_std(th*x*pi/180)'* 180/pi /th);
+x_u68 = (x_med + circ_std(th*x*pi/180)'* 180/pi /th);
+x_l68 = (x_med - circ_std(th*x*pi/180)'* 180/pi /th);
+
+% Make sure all angles are between -180 and 180
+x_med(x_med<0) = x_med(x_med<0) + 180;
+x_u95(x_u95<0) = x_u95(x_u95<0) + 180;
+x_l95(x_l95<0) = x_l95(x_l95<0) + 180;
+x_u68(x_u68<0) = x_u68(x_u68<0) + 180;
+x_l68(x_l68<0) = x_l68(x_l68<0) + 180;
+
+x_med(x_med>180) = x_med(x_med>180) - 180;
+x_u95(x_u95>180) = x_u95(x_u95>180) - 180;
+x_l95(x_l95>180) = x_l95(x_l95>180) - 180;
+x_u68(x_u68>180) = x_u68(x_u68>180) - 180;
+x_l68(x_l68>180) = x_l68(x_l68>180) - 180;
+
+
+end
+
