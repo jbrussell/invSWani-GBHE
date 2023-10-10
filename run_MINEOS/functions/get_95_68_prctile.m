4051c376c44c03f823895844b4d54ad1f7f4ea30
Josh Russell <jbrussell93@gmail.com>
2023-10-10 17:16:03 -0400

    fix bug in 68 confidence interval

--- run_MINEOS/functions/get_95_68_prctile.m
+++ run_MINEOS/functions/get_95_68_prctile.m
@@ -5,8 +5,8 @@ function [ x_med, x_u95, x_l95, x_u68, x_l68 ] = get_95_68_prctile( x )
 %
 x_u95 = prctile(x,97.5,1)';
 x_l95 = prctile(x,2.5,1)';
-x_u68 = prctile(x,16,1)';
-x_l68 = prctile(x,84,1)';
+x_u68 = prctile(x,84,1)';
+x_l68 = prctile(x,16,1)';
 x_med = prctile(x,50,1)';
 
 end
